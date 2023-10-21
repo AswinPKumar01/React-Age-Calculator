79bbdd320d8e0bf51858159a23a5711e45e7ac71
AswinPKumar01 <aswinpkumar03@gmail.com>
2023-10-21 15:03:12 +0530

    Added files for Age Calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const buttonStyle = {
+  background: 'blue',
+  color: 'white',
+  padding: '10px 20px',
+  border: 'none',
+  cursor: 'pointer',
+  borderRadius: '4px',
+  marginTop: '5px'
+};
+
+const App = () => {
+  const [birthDate, setBirthDate] = useState('');
+  const [age, setAge] = useState(null);
+
+  const calculateAge = () => {
+    if (birthDate) {
+      const birthYear = new Date(birthDate).getFullYear();
+      const currentYear = new Date().getFullYear();
+      const calculatedAge = currentYear - birthYear;
+      setAge(calculatedAge);
+    } else {
+      setAge(null);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        border: '2px solid black'
+      }}
+    >
+      <h1>Age Calculator</h1>
+      <form style={{ textAlign: 'center'}}>
+        <label htmlFor="birthDate">Enter your Date of Birth:</label>
+        <br />
+        <input
+          type="date"
+          id="birthDate"
+          value={birthDate}
+          onChange={(e) => setBirthDate(e.target.value)}
+          style={{ margin: '10px 0' , width: '300px', height: '30px', borderRadius: '5px' }}
+        />
+        <br />
+        <button
+          type="button"
+          onClick={calculateAge}
+          style={buttonStyle}
         >
-          Learn React
-        </a>
-      </header>
+          Calculate Age
+        </button>
+      </form>
+      {age !== null && <p> <b> You are {age} years old.</b></p>}
     </div>
   );
-}
+};
 
 export default App;
+
